7e619a58d66da1e50fb98ce85100cd168071ca68
Tanel Metshein <tanemets@tlu.ee>
2026-06-05 01:21:54 +0300

    Refactor mob behavior: rename Timer to DeathTimer, add KnockbackTimer, and implement knockback logic

--- menu.gd
+++ menu.gd
@@ -1,6 +1,9 @@
 extends Control
 
 
+func _ready():
+	preload("res://game.tscn")
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 
--- mob/mob.gd
+++ mob/mob.gd
@@ -5,11 +5,14 @@ signal died()
 var health = 3
 #var speed = 3.0
 var speed = randf_range(2.0, 4.0)
+var is_knocked = false
+
 
 #@onready var bat_model = %bat_model
 @onready var bat_model = $bat_model
 #@onready var timer: Timer = %Timer
-@onready var timer: Timer = $Timer
+@onready var timer: Timer = $DeathTimer
+@onready var knockbackTimer: Timer = $KnockbackTimer
 @onready var hurt_sound: AudioStreamPlayer3D = $HurtSound
 @onready var ko_sound: AudioStreamPlayer3D = $KOSound
 #@onready var collision_shape_3d: CollisionShape3D = %CollisionShape3D
@@ -18,12 +21,18 @@ var speed = randf_range(2.0, 4.0)
 @onready var player = get_node("/root/Game/Player")
 
 func _physics_process(delta: float) -> void:
-	var direction = global_position.direction_to(player.global_position)
-	direction.y = 0.0
-	
-	linear_velocity = direction * speed
-	
-	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + (-0.5*PI)
+	if is_knocked:
+		knockbackTimer.start()
+		is_knocked = false
+	else:
+		var direction = global_position.direction_to(player.global_position)
+		direction.y = 0.0
+		
+		if direction.length_squared() > 0.0:
+			direction = direction.normalized()
+			linear_velocity = direction * speed
+		
+		bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + (-0.5*PI)
 
 func take_damage():
 	if health == 0:
@@ -53,7 +62,19 @@ func take_damage():
 		
 		ko_sound.play()
 	
+func fly_backwards(body):
+	#var away = global_position - player.global_position
+	var away = global_position - body.global_position
+	away.y = 0.0
 	
+	away = away.normalized()
+	
+	var strength = 20 * (1.0 + randf_range(0.0, 5.0))
+	sleeping = false
+	
+	apply_central_impulse(away * strength)
+	
+	is_knocked = true
 
 
 func _on_timer_timeout() -> void:
@@ -62,5 +83,11 @@ func _on_timer_timeout() -> void:
 
 
 func _on_body_entered(body: Node) -> void:
+	if health == 0:
+		return
+		
 	if body.has_method("player_take_damage"):
 		body.player_take_damage()
+	fly_backwards(body)
+		
+	
